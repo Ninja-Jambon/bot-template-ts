9f6eb496fdd9414d448fff403e695dfd573e45ab
Lukian LEIZOUR <lulul7018@gmail.com>
2024-03-10 21:28:46 +0100

    bug resolution

--- src/commands/default/ping.ts
+++ src/commands/default/ping.ts
@@ -1,10 +1,10 @@
-import { SlashCommandBuilder, CommandInteraction } from "discord.js";
+import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("ping")
         .setDescription("Replies with Pong!"),
-    async execute(interaction: CommandInteraction) {
+    async execute(interaction: ChatInputCommandInteraction) {
         await interaction.reply("Pong!");
     },
 };
